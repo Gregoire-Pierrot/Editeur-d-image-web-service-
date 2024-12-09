05fbd1f10cef9e4784c92fbd2838be31d30889dc
Grégoire Pierrot <gregpierr453@gmail.com>
2024-12-09 16:35:08 +0100

    Ajout de la modification d'image par navigateur

--- src/code/serveur/app/views.py
+++ src/code/serveur/app/views.py
@@ -76,7 +76,61 @@ def token():
 
 @app.route("/service", methods=['GET', 'POST'])
 def service():
-    return render_template('service.html', username=session['username'])
+    if 'username' in session:
+        return render_template('service.html', username=session['username'])
+    return redirect(url_for('login'))
+
+@app.route("/modification-web", methods=['GET', 'POST'])
+def modification_web():
+    if 'username' in session:
+        data = request.get_json()
+        modifications = []
+        if 'encoded_image' in data:
+            encoded_image = data['encoded_image']
+            encoded_string = "Rien ne s'est passé."
+            try:
+                image_data = base64.b64decode(encoded_image)
+            except base64.binascii.Error:
+                return {"status": "error", "message": "Image mal encodée"}, 400
+    
+            image_path = 'images/image_recue.png'
+            with open(image_path, 'wb') as image_file:
+                image_file.write(image_data)
+            
+            print("Image recue.")
+            if 'modifications' in data:
+                modifications = data['modifications']
+                for modification in modifications:
+                    if modification == "rotate_left":
+                        rotate(90)
+                    elif modification == "rotate_right":
+                        rotate(270)
+                    elif modification == "inverse":
+                        inverse()
+                    elif modification == "b&w":
+                        bw()
+                    elif modification == "grayscale":
+                        grayscale()
+                    else:
+                        return {"status": "error", "message": "Modification inconnue. Voici les modifications disponible : {mods}"}, 400
+                    print(modification)
+                
+                with open(image_path, "rb") as image_file:
+                    encoded_string = base64.b64encode(image_file.read())
+                try:
+                    os.remove(image_path)
+                    print(f"{image_path} a été supprimé.")
+                except FileNotFoundError:
+                    print(f"Le fichier {image_path} n'existe pas.")
+                except PermissionError:
+                    print(f"Permission refusée pour supprimer {image_path}.")
+                except Exception as e:
+                    print(f"Une erreur s'est produite : {e}")
+                return encoded_string
+            else:
+                return {"status": "error", "message": "Aucune modification donnée."}, 400
+        else:
+            return {"status": "error", "message": "Aucune image donnée."}, 400
 
 @app.route("/modification", methods=['GET', 'POST'])
 def modification():
